5478f9e4c4f85e03e13705a2719a8035c2a720ff
fibrahim30 <fibrahim30@gatech.edu>
2025-04-04 01:24:59 -0400

    Updated run scripts

--- vim/scripts/pt-vim-bilinear.sh
+++ vim/scripts/pt-vim-bilinear.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-conda activate conda_visionmamba
-cd ./projects/VisionMamba/vim;
+
+# conda activate conda_visionmamba
+# cd ./projects/VisionMamba/vim;
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --use_env main.py \
     --model vim_base_patch16_224_bimambav2_bilinear \
--- vim/scripts/pt-vim-zoh.sh
+++ vim/scripts/pt-vim-zoh.sh
@@ -5,7 +5,7 @@ cd ./projects/VisionMamba/vim;
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --use_env main.py \
     --model vim_base_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_middle_cls_token_div2 \
     --batch-size 128 \
-    --drop-path 0.1 \
+    --drop-path 0.0 \
     --weight-decay 0.1 \
     --num_workers 25 \
     --data-path "/Volumes/X10 Pro/datasets/imagenet-1k/train" \
