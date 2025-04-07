0ecac25869dd5c267bca356121ed413b17ac30cb
fibrahim30 <fibrahim30@gatech.edu>
2025-04-06 20:56:52 -0400

    Updated scripts for remote access runs on CVL Servers with two Nvidia A100 GPUs

--- vim/scripts/pt-vim-t.sh
+++ vim/scripts/pt-vim-t.sh
@@ -9,6 +9,6 @@ CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nproc_per_node=2 ./vi
     --drop-path 0.0 \
     --weight-decay 0.1 \
     --num_workers 25 \
-    --data-path /data/fady/datasets/imagenet-1k/train \
+    --data-path /data/fady/datasets/imagenet-1k \
     --output_dir ./output/vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2 \
     --no_amp
