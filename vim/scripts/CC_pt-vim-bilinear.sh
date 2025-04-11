f08b74ca327435fd4bce1eb2bd4dd94d2b43639d
fibrahim30 <fibrahim30@gatech.edu>
2025-04-10 23:23:41 -0400

    Improved memory management on distributed devices / num_workers, batch size, etc

--- vim/scripts/CC_pt-vim-bilinear.sh
+++ vim/scripts/CC_pt-vim-bilinear.sh
@@ -5,10 +5,11 @@
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 ./vim/main.py \
     --model vim_tiny_patch16_224_bimambav2_bilinear \
-    --batch-size 128 \
+    --batch-size 64 \
     --drop-path 0.0 \
     --weight-decay 0.1 \
-    --num_workers 0 \
+    --num_workers 4 \
     --data-path /home/f7ibrahi/projects/def-wangcs/dataset/ImageNet/ILSVRC2012 \
     --output_dir ./output/vim_tiny_patch16_224_bimambav2_bilinear \
-    --no_amp 
+    --no_amp \
+    --pin-memory 
