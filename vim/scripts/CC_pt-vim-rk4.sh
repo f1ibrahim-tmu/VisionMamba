710cb5469b7912358c0658e696f637299492058c
fibrahim30 <fibrahim30@gatech.edu>
2025-04-17 09:38:54 -0400

    Reduced batch size and removed 'no-amp' from script to improve memory usage

--- vim/scripts/CC_pt-vim-rk4.sh
+++ vim/scripts/CC_pt-vim-rk4.sh
@@ -5,10 +5,9 @@
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 ./vim/main.py \
     --model vim_tiny_patch16_224_bimambav2_rk4 \
-    --batch-size 64 \
+    --batch-size 32 \
     --drop-path 0.0 \
     --weight-decay 0.1 \
     --num_workers 4 \
     --data-path /home/f7ibrahi/projects/def-wangcs/dataset/ImageNet/ILSVRC2012 \
-    --output_dir ./output/vim_tiny_patch16_224_bimambav2_rk4 \
-    --no_amp 
+    --output_dir ./output/vim_tiny_patch16_224_bimambav2_rk4
