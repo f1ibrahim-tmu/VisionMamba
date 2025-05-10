8f40b7172d8f3bea3c2234d11cd7c50898091adb
fibrahim30 <fibrahim30@gatech.edu>
2025-05-10 02:12:21 -0400

    Update to CVIS scripts - added long term mem logging, reduced batch size

--- vim/scripts/CVIS/pt-vim-rk4.sh
+++ vim/scripts/CVIS/pt-vim-rk4.sh
@@ -9,9 +9,16 @@ CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nproc_per_node=2 \
     --master_port=0 \
     ./vim/main.py \
     --model vim_tiny_patch16_224_bimambav2_rk4 \
-    --batch-size 128 \
+    --batch-size 64 \
     --drop-path 0.0 \
     --weight-decay 0.1 \
     --num_workers 0 \
     --data-path /data/fady/datasets/imagenet-1k \
-    --output_dir ./output/vim_tiny_rk4
+    --output_dir ./output/vim_tiny_rk4 \
+    --amp
+
+# GPU stats every 8 hours to file
+watch -n 28800 -t 'nvidia-smi >> ./output/vim_tiny_rk4/gpu.log'
+
+# PyTorch memory every 8 hours
+watch -n 28800 -t 'python -c "import torch; print(torch.cuda.memory_summary())" >> ./output/vim_tiny_rk4/torch_mem.log'
--- vim/scripts/CVIS/pt-vim-zoh.sh
+++ vim/scripts/CVIS/pt-vim-zoh.sh
@@ -10,9 +10,16 @@ CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nproc_per_node=2 \
     --master_port=0 \
     ./vim/main.py \
     --model vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2 \
-    --batch-size 128 \
+    --batch-size 64 \
     --drop-path 0.0 \
     --weight-decay 0.1 \
     --num_workers 0 \
     --data-path /data/fady/datasets/imagenet-1k \
-    --output_dir ./output/vim_tiny_zoh
+    --output_dir ./output/vim_tiny_zoh \
+    --amp
+
+# GPU stats every 8 hours to file
+watch -n 28800 -t 'nvidia-smi >> ./output/vim_tiny_zoh/gpu.log'
+
+# PyTorch memory every 8 hours
+watch -n 28800 -t 'python -c "import torch; print(torch.cuda.memory_summary())" >> ./output/vim_tiny_zoh/torch_mem.log'
