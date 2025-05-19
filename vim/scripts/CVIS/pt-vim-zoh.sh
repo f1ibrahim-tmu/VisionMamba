a838884990e1fee227431e0429fd21e31bc0f815
fibrahim30 <fibrahim30@gatech.edu>
2025-05-19 13:56:48 -0400

    Updated pretraining zero order hold for CVIS lab servers

--- vim/scripts/CVIS/pt-vim-zoh.sh
+++ vim/scripts/CVIS/pt-vim-zoh.sh
@@ -3,7 +3,6 @@
 # conda activate conda_visionmamba
 # cd ./projects/VisionMamba/vim;
 
-
 CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nproc_per_node=2 \
     --rdzv-backend=c10d \
     --rdzv-endpoint=localhost:0 \
