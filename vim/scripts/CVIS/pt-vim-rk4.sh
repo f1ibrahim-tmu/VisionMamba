1e08f42bad844f15ed01766e8449d3558fa1d8f0
fibrahim30 <fibrahim30@gatech.edu>
2025-05-19 14:10:23 -0400

    Updated output files for GPU Stats for all run scripts on CVIS Servers

--- vim/scripts/CVIS/pt-vim-rk4.sh
+++ vim/scripts/CVIS/pt-vim-rk4.sh
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nproc_per_node=2 \
     --output_dir ./output/vim_tiny_rk4
 
 # GPU stats every 8 hours to file
-watch -n 28800 -t 'nvidia-smi >> ./output/vim_tiny_rk4_gpu.log'
+watch -n 28800 -t 'nvidia-smi >> ./output/vim_tiny_rk4/vim_tiny_rk4_gpu.log'
 
 # PyTorch memory every 8 hours
 # watch -n 28800 -t 'python -c "import torch; print(torch.cuda.memory_summary())" >> ./output/vim_tiny_rk4_torch_mem.log'
