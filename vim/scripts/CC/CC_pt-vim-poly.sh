036d8768d4b128ae6610c823f93902c8a51fd6c6
fibrahim30 <fibrahim30@gatech.edu>
2025-05-19 16:54:11 -0400

    Updated CC Run Scripts for OOM errors

--- vim/scripts/CC/CC_pt-vim-poly.sh
+++ vim/scripts/CC/CC_pt-vim-poly.sh
@@ -3,11 +3,14 @@
 # conda activate conda_visionmamba
 # cd ./projects/VisionMamba/vim;
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 ./vim/main.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 \
+    --rdzv-backend=c10d \
+    --rdzv-endpoint=$HOSTNAME:0 \
+    ./vim/main.py \
     --model vim_tiny_patch16_224_bimambav2_poly \
-    --batch-size 8 \
+    --batch-size 4 \
     --drop-path 0.0 \
     --weight-decay 0.1 \
-    --num_workers 2 \
+    --num_workers 0 \
     --data-path /home/f7ibrahi/projects/def-wangcs/dataset/ImageNet/ILSVRC2012 \
     --output_dir ./output/vim_tiny_patch16_224_bimambav2_poly
