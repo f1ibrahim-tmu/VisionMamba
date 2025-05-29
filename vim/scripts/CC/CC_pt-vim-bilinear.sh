ca6591ebd86de091393ffb158e247e8fe16306b4
fibrahim30 <fibrahim30@gatech.edu>
2025-05-29 16:22:22 -0400

    reduced batch size, added mixed precision flag

--- vim/scripts/CC/CC_pt-vim-bilinear.sh
+++ vim/scripts/CC/CC_pt-vim-bilinear.sh
@@ -6,9 +6,10 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 \
     ./vim/main.py \
     --model vim_tiny_patch16_224_bimambav2_bilinear \
-    --batch-size 128 \
+    --batch-size 32 \
     --drop-path 0.0 \
     --weight-decay 0.1 \
     --num_workers 0 \
     --data-path /home/f7ibrahi/projects/def-wangcs/dataset/ImageNet/ILSVRC2012 \
-    --output_dir ./output/vim_tiny_bilinear
+    --output_dir ./output/vim_tiny_bilinear \
+    --amp
