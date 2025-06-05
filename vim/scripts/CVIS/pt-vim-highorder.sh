64a05f33780e16ff00d3a95e2e7eb3277a6c89ec
fibrahim30 <fibrahim30@gatech.edu>
2025-06-05 13:50:18 -0400

    more edits to CVIS run scripts

--- vim/scripts/CVIS/pt-vim-highorder.sh
+++ vim/scripts/CVIS/pt-vim-highorder.sh
@@ -3,21 +3,21 @@
 # conda activate conda_visionmamba
 # cd ./projects/VisionMamba/vim;
 
+# GPU stats every 8 hours to file
+watch -n 28800 -t 'nvidia-smi >> ./output/vim_tiny_highorder/vim_tiny_highorder_gpu.log'
+
 CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nproc_per_node=2 \
     --rdzv-backend=c10d \
     --rdzv-endpoint=localhost:0 \
     --master_port=0 \
     ./vim/main.py \
     --model vim_tiny_patch16_224_bimambav2_highorder \
-    --batch-size 64 \
+    --batch-size 128 \
     --drop-path 0.0 \
     --weight-decay 0.1 \
     --num_workers 0 \
     --data-path /data/fady/datasets/imagenet-1k \
     --output_dir ./output/vim_tiny_highorder
-
-# GPU stats every 8 hours to file
-watch -n 28800 -t 'nvidia-smi >> ./output/vim_tiny_highorder/vim_tiny_highorder_gpu.log'
 
 # PyTorch memory every 8 hours
 # watch -n 28800 -t 'python -c "import torch; print(torch.cuda.memory_summary())" >> ./output/vim_tiny_highorder_torch_mem.log'
