c031edbac17c549791c34c8b5fbc4155fe771b73
fibrahim30 <fibrahim30@gatech.edu>
2025-06-06 01:04:57 -0400

    reduced batch size... again

--- vim/scripts/CVIS/pt-vim-bilinear.sh
+++ vim/scripts/CVIS/pt-vim-bilinear.sh
@@ -3,9 +3,6 @@
 # conda activate conda_visionmamba
 # cd ./projects/VisionMamba/vim;
 
-# GPU stats every 8 hours to file
-watch -n 28800 -t 'nvidia-smi >> ./output/vim_tiny_bilinear/vim_tiny_bilinear_gpu.log'
-
 CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nproc_per_node=2 \
     --rdzv-backend=c10d \
     --rdzv-endpoint=localhost:0 \
@@ -18,6 +15,3 @@ CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nproc_per_node=2 \
     --num_workers 0 \
     --data-path /data/fady/datasets/imagenet-1k \
     --output_dir ./output/vim_tiny_bilinear
-
-# PyTorch memory every 8 hours
-# watch -n 28800 -t 'python -c "import torch; print(torch.cuda.memory_summary())" >> ./output/vim_tiny_bilinear_torch_mem.log'
