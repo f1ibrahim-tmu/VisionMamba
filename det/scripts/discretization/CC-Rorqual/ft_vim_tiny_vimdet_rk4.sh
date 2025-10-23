4a039228e74c0d0a44e034b6b1c8c49f008965f4
fibrahim30 <f1ibrahim@torontomu.ca>
2025-10-23 00:15:36 -0400

    Updated scripts for slurm distributed jobs

--- det/scripts/discretization/CC-Rorqual/ft_vim_tiny_vimdet_rk4.sh
+++ det/scripts/discretization/CC-Rorqual/ft_vim_tiny_vimdet_rk4.sh
@@ -5,10 +5,10 @@ DET_CONFIG_NAME=cascade_mask_rcnn_vimdet_t_100ep_adj1_rk4
 DET_CONFIG=projects/ViTDet/configs/COCO/${DET_CONFIG_NAME}.py
 
 CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nproc_per_node=2 \
-    /det/tools/lazyconfig_train_net.py \
-    --num-gpus 4 --num-machines 1 --machine-rank 0 --dist-url "tcp://127.13.44.12:60903" \
+    ./det/tools/lazyconfig_train_net.py \
     --config-file ${DET_CONFIG} \
     train.output_dir=work_dirs/${DET_CONFIG_NAME}-4gpu \
     dataloader.train.num_workers=128 \
     dataloader.test.num_workers=8 \
     model.backbone.discretization_method=rk4
+    # --num-gpus 4 --num-machines 1 --machine-rank 0 --dist-url "tcp://127.13.44.12:60903" \
--- seg/scripts/discretization/CC-Fir/ft_vim_tiny_upernet_highorder.sh
+++ seg/scripts/discretization/CC-Fir/ft_vim_tiny_upernet_highorder.sh
@@ -3,9 +3,10 @@
 
 SEG_CONFIG=configs/vim/upernet/upernet_vim_tiny_24_512_slide_60k_highorder.py
 PRETRAIN_CKPT=home/f7ibrahi/projects/def-wangcs/f7ibrahi/projects/VisionMamba/output/vim_tiny_highorder/best_checkpoint.pth
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 --nnodes=${WORLD_SIZE:-1} --node_rank=${RANK:-0} --master_addr=${MASTER_ADDR:-localhost} --master_port=10297 \
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.run --nproc_per_node=4 --nnodes=${WORLD_SIZE:-1} --node_rank=${RANK:-0} --master_addr=${MASTER_ADDR:-localhost} --master_port=10297 \
-    --use_env train.py --launcher pytorch \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 \
+    ./seg/train.py --launcher slurm \
     ${SEG_CONFIG} \
     --seed 0 --work-dir work_dirs/vimseg-t-highorder --deterministic \
     --options model.backbone.pretrained=${PRETRAIN_CKPT} \
