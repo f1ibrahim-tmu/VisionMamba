e750f2b4f3f95d8b7ffa7f83cde6dd470276b2f9
fibrahim30 <f1ibrahim@torontomu.ca>
2025-12-17 22:38:05 -0500

    Updated all segmentation scripts to point to the correct pretrained checkpoints

--- seg/scripts/discretization/CC-Fir/ft_vim_tiny_upernet_highorder.sh
+++ seg/scripts/discretization/CC-Fir/ft_vim_tiny_upernet_highorder.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Higher-Order Hold discretization for Vision Mamba segmentation on ADE20K
 
-SEG_CONFIG=seg/configs/vim/upernet/upernet_vim_tiny_24_512_slide_60k_highorder.py
-PRETRAIN_CKPT=home/f7ibrahi/projects/def-wangcs/f7ibrahi/projects/VisionMamba/output/vim_tiny_highorder/best_checkpoint.pth
+SEG_CONFIG=./seg/configs/vim/upernet/upernet_vim_tiny_24_512_slide_60k_highorder.py
+PRETRAIN_CKPT=/home/f7ibrahi/projects/def-wangcs/f7ibrahi/projects/VisionMamba/output/classification_logs/vim_tiny_highorder/best_checkpoint.pth
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 --nnodes=${WORLD_SIZE:-1} --node_rank=${RANK:-0} --master_addr=${MASTER_ADDR:-localhost} --master_port=10297 \
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 \
--- seg/scripts/discretization/CC-Fir/ft_vim_tiny_upernet_zoh.sh
+++ seg/scripts/discretization/CC-Fir/ft_vim_tiny_upernet_zoh.sh
@@ -2,7 +2,7 @@
 # Zero Order Hold (ZOH) discretization for Vision Mamba segmentation on ADE20K
 
 SEG_CONFIG=./seg/configs/vim/upernet/upernet_vim_tiny_24_512_slide_60k_zoh.py
-PRETRAIN_CKPT=/home/f7ibrahi/projects/def-wangcs/f7ibrahi/projects/VisionMamba/output/vim_tiny_zoh/best_checkpoint.pth
+PRETRAIN_CKPT=/home/f7ibrahi/projects/def-wangcs/f7ibrahi/projects/VisionMamba/output/classification_logs/vim_tiny_zoh/best_checkpoint.pth
 
 # Conditionally set resume checkpoint if it exists
 CHECKPOINT_PATH=output/segmentation_logs/vim_tiny_vimseg_upernet_zoh/checkpoint.pth
