5a97a97b8014e0d480565922d62d9d22ca18ea7b
fibrahim30 <f1ibrahim@torontomu.ca>
2025-12-26 23:31:36 -0500

    updates to directory structure, and file cleanup

--- vim/scripts/ft-vim-t.sh
+++ vim/scripts/ft-vim-t.sh
@@ -2,7 +2,7 @@
 conda activate <your_env>
 cd <path_to_Vim>/vim;
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --use_env main.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.run --nproc_per_node=8 --master_port=0 main.py \
     --model vim_tiny_patch16_stride8_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2 \
     --batch-size 128 \
     --lr 5e-6 \
