55e7913c4d18ed4a30d6263a0eeb8baf1d6bdf9d
f1ibrahim-tmu <f1ibrahim@torontomu.ca>
2026-01-19 23:31:42 -0500

    Change --launcher pytorch to --launcher slurm in segmentation scripts

--- seg/scripts/discretization/CC-Rorqual/ft_vim_tiny_upernet_zoh.sh
+++ seg/scripts/discretization/CC-Rorqual/ft_vim_tiny_upernet_zoh.sh
@@ -43,7 +43,7 @@ echo "Using MASTER_PORT=$MASTER_PORT for job ${SLURM_JOB_ID:-$$}"
 # CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nproc_per_node=2 --nnodes=${WORLD_SIZE:-1} --node_rank=${RANK:-0} --master_addr=${MASTER_ADDR:-localhost} --master_port=10297 \
 
 CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nproc_per_node=2 --master_port $MASTER_PORT \
-    seg/train.py --launcher pytorch \
+    seg/train.py --launcher slurm \
     ${SEG_CONFIG} \
     --seed 0 --work-dir ./output/segmentation_logs/vim_tiny_vimseg_upernet_zoh --deterministic \
     --options model.backbone.pretrained=${PRETRAIN_CKPT} \
