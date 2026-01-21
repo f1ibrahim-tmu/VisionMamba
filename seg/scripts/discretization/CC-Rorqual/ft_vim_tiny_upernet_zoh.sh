6b51ad4fe7ffcbe25f5d7ecebc02f0435b86eeaa
f1ibrahim-tmu <f1ibrahim@torontomu.ca>
2026-01-20 20:18:23 -0500

    Optimize segmentation scripts: increase batch_size from 8 to 32
    - Added train_dataloader.batch_size=32 override to all segmentation training scripts
    - This increases GPU memory utilization from ~9GB to ~36GB on H100 GPUs
    - Better utilizes the 80GB H100 memory capacity (from 11% to 45% utilization)
    - Expected benefits: faster training, more stable gradients, better convergence
    
    Updated scripts:
    - CC-Fir: all 6 discretization methods (zoh, foh, poly, rk4, highorder, bilinear)
    - CC-Rorqual: all 6 discretization methods
    - CVIS: zoh and foh methods

--- seg/scripts/discretization/CC-Rorqual/ft_vim_tiny_upernet_zoh.sh
+++ seg/scripts/discretization/CC-Rorqual/ft_vim_tiny_upernet_zoh.sh
@@ -49,6 +49,7 @@ CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nproc_per_node=2 --ma
     --seed 0 \
     --work-dir ./output/segmentation_logs/vim_tiny_vimseg_upernet_zoh \
     --options model.backbone.pretrained=${PRETRAIN_CKPT} \
+             train_dataloader.batch_size=32 \
              model.backbone.if_bimamba=False \
              model.backbone.bimamba_type=v2 \
              model.backbone.discretization_method=zoh \
