5b8ec7a0d4a2850e4719adac10c774a69817cc45
f1ibrahim-tmu <f1ibrahim@torontomu.ca>
2026-03-16 15:13:40 -0400

    Fix BiMambaInnerFn to use discretization_method in h100-downstream and cvis-a100

--- mamba-1p1p1-h100-downstream/mamba_ssm/modules/mamba_simple.py
+++ mamba-1p1p1-h100-downstream/mamba_ssm/modules/mamba_simple.py
@@ -220,6 +220,7 @@ class Mamba(nn.Module):
                     self.D.float(),
                     delta_bias=self.dt_proj.bias.float(),
                     delta_softplus=True,
+                    discretization_method=self.discretization_method,
                 )    
             elif self.bimamba_type == "v2":
                 A_b = -torch.exp(self.A_b_log.float())
