c0d6c728916ca2c2d90dcd072ec0c19e258d7be5
f1ibrahim-tmu <f1ibrahim@torontomu.ca>
2026-03-27 23:34:03 -0400

    ViMDet: DDP find_unused_parameters=True and FP32 training default
    - Set train.ddp.find_unused_parameters=True to align with seg ViM and avoid
      DDP failures when some parameters skip gradients in a step.
    - Disable AMP (train.amp.enabled=False) for stabler numerics with Mamba and
      alternate discretizations; document how to re-enable BF16 on H100.
    
    Affects all configs that inherit cascade_mask_rcnn_vimdet_b_100ep, including
    CC-Rorqual adj1 discretization runs.

--- det/projects/ViTDet/configs/COCO/mask_rcnn_vimdet_b_100ep.py
+++ det/projects/ViTDet/configs/COCO/mask_rcnn_vimdet_b_100ep.py
@@ -1,5 +1,4 @@
 from functools import partial
-import torch
 from fvcore.common.param_scheduler import MultiStepParamScheduler
 
 from detectron2 import model_zoo
@@ -14,9 +13,12 @@ model = model_zoo.get_config("common/models/mask_rcnn_vimdet.py").model
 
 # Initialization and trainer settings
 train = model_zoo.get_config("common/train.py").train
-train.amp.enabled = True
-train.amp.precision = torch.bfloat16  # BF16 for H100 stability (wider exponent range than FP16)
-train.ddp.fp16_compression = False  # Disable FP16 gradient compression when using BF16 AMP
+# FP32 training: more stable for Mamba + alternate discretizations than BF16 AMP (see CC-Rorqual ViMDet runs).
+# Re-enable AMP for speed if stable: import torch; train.amp.enabled = True; train.amp.precision = torch.bfloat16
+train.amp.enabled = False
+train.ddp.fp16_compression = False
+# Align with seg ViM: avoid DDP errors when some params skip gradients in a step.
+train.ddp.find_unused_parameters = True
 train.init_checkpoint = (
     "detectron2://ImageNetPretrained/MAE/mae_pretrain_vit_base.pth?matching_heuristics=True"
 )
